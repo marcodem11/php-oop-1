69f6ec9eed5a945075b58386c4ff5f346c60c4c6
marcodem11 <marcodemicheleta01@outlook.it>
2022-04-28 16:08:43 +0200

    aggiunte movie e colore background

--- index.php
+++ index.php
@@ -8,7 +8,24 @@
     'Fantasy',
     'Matt Reeves'
 );
+
+$morbius = new Movie(
+    'https://mr.comingsoon.it/imgdb/locandine/235x336/58110.jpg',
+    'Morbius',
+    '2022',
+    'Daniel Espinosa',
+    'Daniel Espinosa'
+);
+
+$uncharted = new Movie(
+    'https://mr.comingsoon.it/imgdb/locandine/235x336/50817.jpg',
+    'Uncharted',
+    '2022',
+    'Avventura - Azione',
+    'Ruben Fleischer'
+);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,6 +50,34 @@
         <span><?php echo $batman->language ?></span>
         <span><?php echo $batman->director ?></span>
         <span><?php echo $batman->releaseDate ?></span>
+      </div>
+    </div>
+
+    <div class="card-wrap">
+      <figure>
+        <img src="<?php echo $morbius->poster ?>" alt="">
+      </figure>
+      <div class="info">
+        <h2>
+          <?php echo $morbius->title ?>
+        </h2>
+        <span><?php echo $morbius->language ?></span>
+        <span>Directed by: <a href="https://www.mymovies.it/film/2022/morbius/" target="_blank""><?php echo $morbius->director ?></a></span>
+        <span>Release date: <?php echo $morbius->exist() ?></span>
+      </div>
+    </div>
+
+    <div class="card-wrap">
+      <figure>
+        <img src="<?php echo $uncharted->poster ?>" alt="">
+      </figure>
+      <div class="info">
+        <h2>
+          <?php echo $uncharted->title ?>
+        </h2>
+        <span><?php echo $uncharted->language ?></span>
+        <span>Directed by: <a href="https://www.mymovies.it/film/2022/uncharted/" target="_blank""><?php echo $uncharted->director ?></a></span>
+        <span>Release date: <?php echo $uncharted->exist() ?></span>
       </div>
     </div>
 
