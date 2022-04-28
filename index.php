fa394235c917dfed84c15d25d632093e9a1a3c1e
marcodem11 <marcodemicheleta01@outlook.it>
2022-04-28 15:49:09 +0200

    inseit movie batman

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php
   require_once __DIR__.'/movie.php';
+
+  $batman = new Movie(
+    'https://mr.comingsoon.it/imgdb/locandine/235x336/57933.jpg',
+    'The Batman',
+    '2022',
+    'Fantasy',
+    'Matt Reeves'
+);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+</head>
+<body>
+  <div class="container">
+    <div class="card-wrap">
+      <figure>
+        <img src="<?php echo $batman->poster ?>" alt="">
+      </figure>
+      <div class="info">
+        <h2>
+          <?php echo $batman->title ?>
+        </h2>
+        <span><?php echo $batman->language ?></span>
+        <span><?php echo $batman->director ?></span>
+        <span><?php echo $batman->releaseDate ?></span>
+      </div>
+    </div>
+
+  </div>
+</body>
+</html> 
--- movie.php
+++ movie.php
@@ -2,12 +2,14 @@
   class movie {
     public $poster;
     public $title;
+    public $language;
     public $director;
     public $releaseDate;
 
-    function __construct(string $_poster, string $_title, string $_director, string $_releaseDate){
+    function __construct(string $_poster, string $_title, string $_language, string $_director, string $_releaseDate){
       $this->poster = $_poster;
       $this->title = $_title;
+      $this->language = $_language;
       $this->director = $_director;
       $this->releaseDate = $_releaseDate;
     }
